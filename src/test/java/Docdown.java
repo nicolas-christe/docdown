b757d7430bc3bbf211f1feef4a473011cb8ac485
Nicolas Christe <nicolas.christe@parrot.com>
2014-05-28 23:42:22 +0200

    add test class that generate project documentation

---
+++ src/test/java/Docdown.java
@@ -0,0 +1,28 @@
+/*
+ * Copyright (C) 2013-2013 Nicolas Christe
+ * Copyright (C) 2013-2013 Parrot S.A.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ *
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+
+import com.parrot.docdown.DocdownDoclet;
+
+public class Docdown {
+    static public void main(String[] args) {
+        com.sun.tools.javadoc.Main.execute(new String[]{"-doclet", DocdownDoclet.class.getName(), "-link",
+                "http://docs.oracle.com/javase/7/docs/api/", "-t", "Docdown Doclet", "-d", "build/docdown",
+                "-sourcepath", "src/main/java", "-docsourcepath", "doc", "-subpackages", "com.parrot.docdown"});
+    }
+}
